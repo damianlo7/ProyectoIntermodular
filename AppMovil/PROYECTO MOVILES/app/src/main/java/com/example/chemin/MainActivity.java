5b9e9171244bc086acee1187988fea02989a3a86
damian <damian@gmail.com>
2026-01-19 18:28:15 +0100

    changes

--- AppMovil/PROYECTO MOVILES/app/src/main/java/com/example/chemin/MainActivity.java
+++ AppMovil/PROYECTO MOVILES/app/src/main/java/com/example/chemin/MainActivity.java
@@ -1,20 +1,32 @@
 package com.example.chemin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText usuario;
     EditText contrasenha;
     Button registrarse;
+    Button oldpass;
+    Button btnentrar;
+    ArrayList<Usuario> usuarios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +42,69 @@ public class MainActivity extends AppCompatActivity {
         usuario = findViewById(R.id.usuario);
         contrasenha = findViewById(R.id.contrasenha);
         registrarse = findViewById(R.id.registrarse);
+        oldpass = findViewById(R.id.olpass);
+        btnentrar = findViewById(R.id.btnentrar);
+
+
+        ActivityResultLauncher<Intent> launcher = registerForActivityResult(new
+                ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+
+                Intent intent=result.getData();
+
+                if (intent != null){
+                    if (intent.hasExtra("usuarioNuevo")){
+                        Usuario u = (Usuario) intent.getSerializableExtra("usuarioNuevo");
+                        usuarios.add(u);
+                    }
+                }
+            }
+        });
+
+        btnentrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                if (){
+//
+//                }
+            }
+        });
+
+
+        oldpass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+        registrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CrearCuenta.class);
+                launcher.launch(intent);
+            }
+        });
+
+        oldpass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), restaurarContrasenha.class);
+                launcher.launch(intent);
+            }
+        });
+
+
+
+
+
+
+
+
+
         
 
     }
